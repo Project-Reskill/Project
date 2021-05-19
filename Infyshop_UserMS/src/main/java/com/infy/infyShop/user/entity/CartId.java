9ed3d37bf21a34c58eef16bc76b389ec8c15b825
srajan <srajan.gupta@infosys.com>
2021-05-20 00:28:10 +0530

    Deleted User MS

--- Infyshop_UserMS/src/main/java/com/infy/infyShop/user/entity/CartId.java
+++
@@ -1,8 +0,0 @@
-package com.infy.infyShop.user.entity;
-
-import java.io.Serializable;
-
-public class CartId implements Serializable{
-	private Integer buyerId;
-	private Integer prodId;
-}
--- Infyshop_UserMS/src/main/java/com/infy/infyShop/user/entity/WishlistId.java
+++
@@ -1,8 +0,0 @@
-package com.infy.infyShop.user.entity;
-
-import java.io.Serializable;
-
-public class WishlistId implements Serializable{
-	private Integer buyerId;
-	private Integer prodId;
-}
