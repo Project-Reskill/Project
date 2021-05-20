68167f0db914fb4eb2c8a282a4617befd4d57cf8
srajan <srajan.gupta@infosys.com>
2021-05-20 09:38:31 +0530

    deleted User MS

--- InfyShop_UserMS/src/main/java/com/infy/infyShop/user/service/BuyerService.java
+++
@@ -1,8 +0,0 @@
-package com.infy.infyShop.user.service;
-
-import com.infy.infyShop.user.dto.BuyerDTO;
-
-public interface BuyerService {
-	public String userRegister(BuyerDTO buyerdto);
-	public String userLogin(BuyerDTO buyerDTO);
-}
